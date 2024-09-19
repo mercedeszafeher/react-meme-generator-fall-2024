6e3f9ef03eba16a3c275d9b6167a73a768b5993d
mercedeszafeher <mercedesz.a.feher@gmail.com>
2024-09-19 22:33:33 +0200

    Deleting not needed files and getting started with the project

---
+++ src/MemeGenerator.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+const [topText, setTopText] = useState('');
+const [bottomText, setBottomText] = useState('');
+const [template, setTemplate] = useState('buzz');
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './index.css';
+import './styles/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
